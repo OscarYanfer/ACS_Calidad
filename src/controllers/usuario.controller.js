de80a44c0a234245bc8b69d3cefe277fa24ab704
arnold <luizarnolch@gmai.com>
2021-06-27 22:31:47 -0500

    jalar admin

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -3,7 +3,8 @@ const pool = require('../database');
 
 usuarioGet = async(req, res) => {
     const table = await pool.query("SELECT * FROM pcso_applicants");
-    res.render('admin/list', { table });
+    const Admin = await pool.query("SELECT * FROM pcso_admin");
+    res.render('admin/list', { table, Admin: admin[0] });
     return next();
 }
 
