6b02af093de5dd59d581a0c2f868e8d1ffa34efe
arnold <luizarnolch@gmai.com>
2021-07-10 11:47:11 -0500

    DB deploy config

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,22 @@
 //la configuración de la base de datos
 
 module.exports = {
+
+    database: {
+        host: 'us-cdbr-east-04.cleardb.com',
+        user: 'b27fe36cc8fe8c',
+        password: 'f5f764ff',
+        database: 'heroku_f8f49523a25a3a5'
+    }
+
+    /*
     database: {
         host: 'localhost',
         user: 'root',
         password: 'FisiCalidad',
         database: 'pcs_orange'
-    }
+    } 
+    */
 };
+
+// mysql: //b27fe36cc8fe8c:f5f764ff@us-cdbr-east-04.cleardb.com/heroku_f8f49523a25a3a5?reconnect=true
