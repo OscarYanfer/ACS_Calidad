03535eaf8a2260d1f1cbba5fbb9abf156000408b
Rolando Jesus Zafra Moran <rolando20022016@hotmail.com>
2021-08-05 16:45:01 -0500

    back hotfix

--- src/keys.js
+++ src/keys.js
@@ -9,14 +9,14 @@ module.exports = {
         database: 'heroku_f8f49523a25a3a5'
     }
 
-    /*
-    database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'FisiCalidad',
-        database: 'pcs_orange'
-    } 
-    */
+    
+    // database: {
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: 'FisiCalidad',
+    //     database: 'pcs_orange'
+    // } 
+    
 };
 
 // mysql: //b27fe36cc8fe8c:f5f764ff@us-cdbr-east-04.cleardb.com/heroku_f8f49523a25a3a5?reconnect=true
